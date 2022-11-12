8c0fe227bd41b17e3403b280c6a53ccc111d0ac8
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-11-12 18:48:49 +0300

    more jinja templates

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Jinja template"""
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def display_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def message(text):
+    return 'C %s' % escape(text).replace('_', ' ')
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def display_python(text='is cool'):
+    return 'Python %s' % escape(text).replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def display_num(n):
+    return '%d is a number' % n
+
+
+@app.route('/number_template/<int:n>')
+def number__template(n):
+    return render_template('5-number.html', num=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', num=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
